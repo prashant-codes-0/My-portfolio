eaf86ea0b6c5506b1ea34682856a11e4066ec75b
Prashant Chaudhary <50772407+beginneralways@users.noreply.github.com>
2023-10-21 08:52:17 +0545

    Update projectController.js
    (Added methods to delete and getAllProjects)

--- controller/projectController.js
+++ controller/projectController.js
@@ -1,4 +1,7 @@
 const Project = require('../models/Project');
+const Project = require('../models/Project');
+const { authenticateJWT, checkAdminRole } = require('../middleware/auth');
+
 
 // Get all projects with only necessary fields
 exports.getAllProjects = async (req, res) => {
@@ -27,3 +30,29 @@ exports.createProject = async (req, res) => {
     res.status(500).json({ error: 'Unable to create project' });
   }
 };
+
+
+
+// Get all projects with only necessary fields
+exports.getAllProjects = [authenticateJWT, async (req, res) => {
+  try {
+    const projects = await Project.find({}, '_id title description screenshotUrls').exec();
+    res.json(projects);
+  } catch (error) {
+    res.status(500).json({ error: 'Unable to fetch projects' });
+  }
+}];
+
+// Delete a project by ID (authenticated and with admin role)
+exports.deleteProjectById = [authenticateJWT, checkAdminRole, async (req, res) => {
+  const projectId = req.params.id;
+  try {
+    const project = await Project.findByIdAndRemove(projectId);
+    if (!project) {
+      return res.status(404).json({ error: 'Project not found' });
+    }
+    return res.json({ message: 'Project deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Unable to delete project' });
+  }
+}];
